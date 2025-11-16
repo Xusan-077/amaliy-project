f7b9f8d2e4da4d452ad1292bc69b6827ee03c3f2
XusanYarashov <xusanyarashov1@gmail.com>
2025-11-16 22:07:29 +0500

    added error modal

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -9,6 +9,11 @@ export default function DetailPage({}) {
 
   const [show, setShow] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const [modal, setModal] = useState(false);
+
   function handleAddtoLike() {
     const inLike = like.find((el) => el.title == newData.title);
 
@@ -20,87 +25,130 @@ export default function DetailPage({}) {
   }
 
   async function get() {
-    const res =
-      await axios.get(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6bca5b2d6e8b4eb8b7a4d9fc7332a61c
-  `);
+    try {
+      setIsLoading(true);
+      const res =
+        await axios.get(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=6bca5b2d6e8b4eb8b7a4d9fc7332a61c
+        `);
 
-    setNews(res.data.articles);
+      setNews(res.data.articles);
+      setIsLoading(false);
+    } catch (err) {
+      setError(err.message || "Getda xatolik");
+    } finally {
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
     get();
-  }, []);
+
+    if (error) {
+      setModal(true);
+    } else {
+      setModal(false);
+    }
+  }, [error]);
 
   return (
-    <section className="detail">
-      <div className="container">
-        <div className="detail__inner">
-          <div className="detail__right">
-            <h2 className="detail__title">{newData.title}</h2>
-            <div className="detail__top">
-              <span className="detail__source">{newData.source.name}</span>
-              <div className="detail__like" onClick={handleAddtoLike}>
-                <i
-                  className={`bi ${
-                    like.find((el) => el.title === newData.title)
-                      ? "bi-heart-fill active"
-                      : "bi-heart"
-                  } text-[18px]`}
-                ></i>
-              </div>
+    <>
+      {modal && (
+        <div className="modal">
+          <div className="modal__content">
+            <div className="modal__top">
+              <span className="modal__title">Error</span>
             </div>
-
-            <div className="detail__img">
-              <img src={newData.urlToImage} alt="" className="detai__img" />
+            <div className="modal__bottom">
+              <p className="modal__text">{error}</p>
             </div>
-
-            <p className="detail__description">{newData.description}</p>
-            <p className="detail__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              aliquam minima ab provident, in corrupti ea adipisci modi
-              excepturi iste tempore architecto, quae quas officia libero.
-              Eveniet sint pariatur corrupti omnis asperiores. Nesciunt corrupti
-              aliquid minima sapiente ad quisquam error quo, in, earum deserunt
-              sint exercitationem saepe cum recusandae architecto voluptatem
-              quos temporibus aliquam voluptatum? Eos, recusandae, dolore quasi,
-              accusantium ut tempora ipsa officiis pariatur accusamus nihil ea
-              officia soluta sunt quaerat quibusdam praesentium aut laborum
-              possimus nulla magni atque corporis quas ratione! Nam cumque
-              aliquid, provident, aliquam placeat suscipit alias molestias
-              saepe, corporis numquam exercitationem vero expedita impedit
-              assumenda!
-            </p>
-            <p className="detail__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              aliquam minima ab provident, in corrupti ea adipisci modi
-              excepturi iste tempore architecto, quae quas officia libero.
-              Eveniet sint pariatur corrupti omnis asperiores. Nesciunt corrupti
-              aliquid minima sapiente ad quisquam error quo, in, earum deserunt
-              sint exercitationem saepe cum recusandae architecto voluptatem
-              quos temporibus aliquam voluptatum? Eos, recusandae, dolore quasi,
-              accusantium ut tempora ipsa officiis pariatur accusamus nihil ea
-              officia soluta sunt quaerat quibusdam praesentium aut laborum
-              possimus nulla magni atque corporis quas ratione! Nam cumque
-              aliquid, provident, aliquam placeat suscipit alias molestias
-              saepe, corporis numquam exercitationem vero expedita impedit
-              assumenda!
-            </p>
-          </div>
-          <div className="detail__left">
-            <ul
-              className={`${show ? "detail__list--active" : "detail__list"} `}
-            >
-              {news.map((el, index) => (
-                <CommentItem item={el} key={index} {...el} />
-              ))}
-            </ul>
-
-            <button onClick={() => setShow(true)} className="show-more">
-              Show All
-            </button>
           </div>
         </div>
-      </div>
-    </section>
+      )}
+      <section className="detail">
+        <div className="container">
+          <div className="detail__inner">
+            <div className="detail__right">
+              <h2 className="detail__title">{newData.title}</h2>
+              <div className="detail__top">
+                <span className="detail__source">{newData.source.name}</span>
+                <div className="detail__like" onClick={handleAddtoLike}>
+                  <i
+                    className={`bi ${
+                      like.find((el) => el.title === newData.title)
+                        ? "bi-heart-fill active"
+                        : "bi-heart"
+                    } text-[18px]`}
+                  ></i>
+                </div>
+              </div>
+
+              <div className="detail__img">
+                <img src={newData.urlToImage} alt="" className="detai__img" />
+              </div>
+
+              <p className="detail__description">{newData.description}</p>
+              <p className="detail__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                aliquam minima ab provident, in corrupti ea adipisci modi
+                excepturi iste tempore architecto, quae quas officia libero.
+                Eveniet sint pariatur corrupti omnis asperiores. Nesciunt
+                corrupti aliquid minima sapiente ad quisquam error quo, in,
+                earum deserunt sint exercitationem saepe cum recusandae
+                architecto voluptatem quos temporibus aliquam voluptatum? Eos,
+                recusandae, dolore quasi, accusantium ut tempora ipsa officiis
+                pariatur accusamus nihil ea officia soluta sunt quaerat
+                quibusdam praesentium aut laborum possimus nulla magni atque
+                corporis quas ratione! Nam cumque aliquid, provident, aliquam
+                placeat suscipit alias molestias saepe, corporis numquam
+                exercitationem vero expedita impedit assumenda!
+              </p>
+              <p className="detail__description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                aliquam minima ab provident, in corrupti ea adipisci modi
+                excepturi iste tempore architecto, quae quas officia libero.
+                Eveniet sint pariatur corrupti omnis asperiores. Nesciunt
+                corrupti aliquid minima sapiente ad quisquam error quo, in,
+                earum deserunt sint exercitationem saepe cum recusandae
+                architecto voluptatem quos temporibus aliquam voluptatum? Eos,
+                recusandae, dolore quasi, accusantium ut tempora ipsa officiis
+                pariatur accusamus nihil ea officia soluta sunt quaerat
+                quibusdam praesentium aut laborum possimus nulla magni atque
+                corporis quas ratione! Nam cumque aliquid, provident, aliquam
+                placeat suscipit alias molestias saepe, corporis numquam
+                exercitationem vero expedita impedit assumenda!
+              </p>
+            </div>
+            <div className="detail__left">
+              <ul
+                className={`${show ? "detail__list--active" : "detail__list"} `}
+              >
+                {isLoading
+                  ? Array.from({ length: 10 }).map((_, index) => (
+                      <li key={index} className="home__item skeleton-item">
+                        <div className="item__img-div">
+                          <div className="skeleton skeleton-img"></div>
+                        </div>
+                        <div>
+                          <h2 className="skeleton skeleton-title"></h2>
+                          <div className="item__bottom">
+                            <span className="skeleton skeleton-source"></span>
+                            <span className="skeleton skeleton-date"></span>
+                          </div>
+                        </div>
+                      </li>
+                    ))
+                  : news.map((el, index) => (
+                      <CommentItem item={el} key={index} {...el} />
+                    ))}
+              </ul>
+
+              <button onClick={() => setShow(true)} className="show-more">
+                Show All
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,6 +15,8 @@ export default function Home() {
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
+  const [modal, setModal] = useState(false);
+
   async function getNews() {
     try {
       setIsLoading(true);
@@ -26,7 +28,7 @@ export default function Home() {
 
       setNewNews(res.data.articles);
     } catch (err) {
-      setError(err.massage);
+      setError(err.message || "getda xatolik");
     } finally {
       setIsLoading(false);
     }
@@ -43,7 +45,7 @@ export default function Home() {
 
       setNews(res.data.articles);
     } catch (err) {
-      setError(err.massage);
+      setError(err.message || "getda xatolik");
     } finally {
       setIsLoading(false);
     }
@@ -61,7 +63,7 @@ export default function Home() {
 
       setApple(res.data.articles);
     } catch (err) {
-      setError(err.massage);
+      setError(err.message || "getda xatolik");
     } finally {
       setIsLoading(false);
     }
@@ -79,123 +81,186 @@ https://newsapi.org/v2/everything?q=tesla&from=2025-10-16&sortBy=publishedAt&api
 
       setTesla(res.data.articles);
     } catch (err) {
-      setError(err.massage);
+      setError(err.message || "getda xatolik");
     } finally {
       setIsLoading(false);
     }
   }
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     get();
     getNews();
     getApple();
     getTesla();
-  }, []);
+
+    if (error) {
+      setModal(true);
+    }
+  }, [error]);
 
   return (
     <>
-      <section className="home">
-        <div className="container">
-          <div className="home__inner">
-            <div className="home__inner__top">
-              <h2 className="home__title">{t("main.titel")}</h2>
-              <div className="home__content">
-                <div className="home__right">
-                  <ul className="home__list">
-                    {isLoading
-                      ? Array.from({ length: 10 }).map((_, index) => (
-                          <li key={index} className="home__item skeleton-item">
-                            <div className="item__img-div">
-                              <div className="skeleton skeleton-img"></div>
-                            </div>
-                            <div>
-                              <h2 className="skeleton skeleton-title"></h2>
-                              <div className="item__bottom">
-                                <span className="skeleton skeleton-source"></span>
-                                <span className="skeleton skeleton-date"></span>
-                              </div>
-                            </div>
-                          </li>
-                        ))
-                      : error
-                      ? `${error}`
-                      : news.map((el, index) =>
-                          index < 14 ? (
-                            <CommentItem
-                              news={news}
-                              item={el}
-                              id={index + 1}
-                              key={index}
-                              {...el}
-                            />
-                          ) : (
-                            ""
-                          )
-                        )}
-                  </ul>
-                </div>
-              </div>
+      {modal && (
+        <div className="modal">
+          <div className="modal__content">
+            <div className="modal__top">
+              <span className="modal__title">Error</span>
             </div>
-            <div className="home__inner__bottom">
-              <h2 className="home__title">{t("main.secontTitle")}</h2>
-
-              <div className="home__content">
-                <div className="home__right">
-                  <ul className="home__list--active">
-                    {isLoading
-                      ? Array.from({ length: 10 }).map((_, index) => (
-                          <li key={index} className="home__item skeleton-item">
-                            <div className="item__img-div">
-                              <div className="skeleton skeleton-img"></div>
-                            </div>
-                            <div>
-                              <h2 className="skeleton skeleton-title"></h2>
-                              <div className="item__bottom">
-                                <span className="skeleton skeleton-source"></span>
-                                <span className="skeleton skeleton-date"></span>
-                              </div>
-                            </div>
-                          </li>
-                        ))
-                      : error
-                      ? `${error}`
-                      : newNews?.map((el, index) =>
-                          index < 5 ? (
-                            <CommentItem
-                              thin
-                              news={news}
-                              item={el}
-                              id={index + 1}
-                              key={index}
-                              {...el}
-                            />
-                          ) : (
-                            ""
-                          )
-                        )}
-                  </ul>
-                </div>
-              </div>
+            <div className="modal__bottom">
+              <p className="modal__text">{error}</p>
             </div>
           </div>
-          <div className="home__top">
-            <div className="otherNews__div">
-              <h2 className="home__title">Apple</h2>
+        </div>
+      )}
 
-              <ul className="otherNews__list">
-                {apple?.map((el, index) =>
-                  index < 5 ? <OtherNews item={el} key={index} {...el} /> : ""
-                )}
-              </ul>
+      <section className="web">
+        <div className="container">
+          <div className="web__inner">
+            <div className="home__inner">
+              <div className="home__inner__top">
+                <h2 className="home__title">{t("main.titel")}</h2>
+                <div className="home__content">
+                  <div className="home__right">
+                    <ul className="home__list">
+                      {isLoading
+                        ? Array.from({ length: 14 }).map((_, index) => (
+                            <li
+                              key={index}
+                              className="home__item skeleton-item"
+                            >
+                              <div className="item__img-div">
+                                <div className="skeleton skeleton-img"></div>
+                              </div>
+                              <div>
+                                <h2 className="skeleton skeleton-title"></h2>
+                                <div className="item__bottom">
+                                  <span className="skeleton skeleton-source"></span>
+                                  <span className="skeleton skeleton-date"></span>
+                                </div>
+                              </div>
+                            </li>
+                          ))
+                        : error
+                        ? `${error}`
+                        : news.map((el, index) =>
+                            index < 14 ? (
+                              <CommentItem
+                                news={news}
+                                item={el}
+                                id={index + 1}
+                                key={index}
+                                {...el}
+                              />
+                            ) : (
+                              ""
+                            )
+                          )}
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="home__inner__bottom">
+                <h2 className="home__title">{t("main.secontTitle")}</h2>
+
+                <div className="home__content">
+                  <div className="home__right">
+                    <ul className="home__list--active">
+                      {isLoading
+                        ? Array.from({ length: 5 }).map((_, index) => (
+                            <li
+                              key={index}
+                              className="home__item skeleton-item"
+                            >
+                              <div className="item__img-div">
+                                <div className="skeleton skeleton-img"></div>
+                              </div>
+                              <div>
+                                <h2 className="skeleton skeleton-title"></h2>
+                                <div className="item__bottom">
+                                  <span className="skeleton skeleton-source"></span>
+                                  <span className="skeleton skeleton-date"></span>
+                                </div>
+                              </div>
+                            </li>
+                          ))
+                        : error
+                        ? `${error}`
+                        : newNews?.map((el, index) =>
+                            index < 5 ? (
+                              <CommentItem
+                                thin
+                                news={news}
+                                item={el}
+                                id={index + 1}
+                                key={index}
+                                {...el}
+                              />
+                            ) : (
+                              ""
+                            )
+                          )}
+                    </ul>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="otherNews__div">
-              <h2 className="home__title">Tesla</h2>
+            <div className="home__top">
+              <div className="otherNews__div">
+                <h2 className="home__title">Apple</h2>
 
-              <ul className="otherNews__list">
-                {tesla?.map((el, index) =>
-                  index < 5 ? <OtherNews item={el} key={index} {...el} /> : ""
-                )}
-              </ul>
+                <ul className="otherNews__list">
+                  {isLoading
+                    ? Array.from({ length: 5 }).map((_, index) => (
+                        <li key={index} className="home__item skeleton-item">
+                          <div className="item__img-div">
+                            <div className="skeleton skeleton-img"></div>
+                          </div>
+                          <div>
+                            <h2 className="skeleton skeleton-title"></h2>
+                            <div className="item__bottom">
+                              <span className="skeleton skeleton-source"></span>
+                              <span className="skeleton skeleton-date"></span>
+                            </div>
+                          </div>
+                        </li>
+                      ))
+                    : apple?.map((el, index) =>
+                        index < 5 ? (
+                          <OtherNews item={el} key={index} {...el} />
+                        ) : (
+                          ""
+                        )
+                      )}
+                </ul>
+              </div>
+              <div className="otherNews__div">
+                <h2 className="home__title">Tesla</h2>
+
+                <ul className="otherNews__list">
+                  {isLoading
+                    ? Array.from({ length: 5 }).map((_, index) => (
+                        <li key={index} className="home__item skeleton-item">
+                          <div className="item__img-div">
+                            <div className="skeleton skeleton-img"></div>
+                          </div>
+                          <div>
+                            <h2 className="skeleton skeleton-title"></h2>
+                            <div className="item__bottom">
+                              <span className="skeleton skeleton-source"></span>
+                              <span className="skeleton skeleton-date"></span>
+                            </div>
+                          </div>
+                        </li>
+                      ))
+                    : tesla?.map((el, index) =>
+                        index < 5 ? (
+                          <OtherNews item={el} key={index} {...el} />
+                        ) : (
+                          ""
+                        )
+                      )}
+                </ul>
+              </div>
             </div>
           </div>
         </div>
